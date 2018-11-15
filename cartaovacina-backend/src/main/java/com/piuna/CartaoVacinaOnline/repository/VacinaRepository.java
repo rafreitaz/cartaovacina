630000a36eaacc71f41e6bd90fa0a2b85d3fc9d0
renato <renato.freitaz@gmail.com>
2018-11-15 12:35:06 -0200

    PrimeNG

--- cartaovacina-backend/src/main/java/com/piuna/CartaoVacinaOnline/repository/VacinaRepository.java
+++ cartaovacina-backend/src/main/java/com/piuna/CartaoVacinaOnline/repository/VacinaRepository.java
@@ -12,4 +12,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface VacinaRepository extends JpaRepository<Vacina, Long> {
+
 }
